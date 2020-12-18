d0298f6fcdc6a38d7ebc956dbecbba53563173a9
Nazar Kuziv <nazar123kuziv@gmail.com>
2020-12-18 18:02:53 +0200

    UnitTest

---
+++ UnitTest 6.2-1/UnitTest 6.2-1.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Project 6.2-1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest621
+{
+	TEST_CLASS(UnitTest621)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int t[6] = { 1, 2, 3, 4, 5, 6 };
+
+			int Min = Search(t, 6);
+
+			Assert::AreEqual(Min, 2);
+		}
+	};
+}
